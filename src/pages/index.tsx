6e5a107aea85e2e42d72f8443c4f05415875caad
reasat-rafio <rafioar96@gmail.com>
2021-03-23 02:47:00 +0600

    server fixerd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ export default function Home({ coverImg }) {
             <link rel="icon" href="/favicon.ico" />
          </Head>
          <main className="w-full">
-            {/* <HomePoster coverImg={coverImg} /> */}
-            {/* <BrowseByCategory /> */}
+            <HomePoster coverImg={coverImg} />
+            <BrowseByCategory />
          </main>
          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero,
          sapiente ducimus soluta suscipit sint quis molestias voluptates at
@@ -50,9 +50,7 @@ export default function Home({ coverImg }) {
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-   const { data } = await axios.get(
-      `https://sunnah-station.herokuapp.com/main-cover-images`
-   );
+   const { data } = await axios.get(`${process.env.URL}/main-cover-images`);
    return {
       props: { coverImg: data[0].img },
    };
