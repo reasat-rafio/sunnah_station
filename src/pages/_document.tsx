b405f80745c53078fdea144d1b6114d700d4b1e8
reasat-rafio <rafioar96@gmail.com>
2021-03-26 16:35:14 +0600

    fav icon added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,27 @@ class MyDocument extends Document {
                   httpEquiv="Content-Type"
                   content="text/html; charset=utf-8"
                />
+
+               <link
+                  rel="apple-touch-icon"
+                  sizes="180x180"
+                  href="/apple-touch-icon.png"
+               />
+               <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="32x32"
+                  href="/favicon-32x32.png"
+               />
+               <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="16x16"
+                  href="/favicon-16x16.png"
+               />
+               <link rel="manifest" href="/site.webmanifest" />
+               <meta name="msapplication-TileColor" content="#2b5797" />
+               <meta name="theme-color" content="#ffffff"></meta>
             </Head>
             <body>
                <Main />
