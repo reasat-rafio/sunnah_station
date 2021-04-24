6ee9d10de51a69ac0278bbc4732a5596332f2200
reasat-rafio <rafioar96@gmail.com>
2021-04-24 17:08:03 +0600

    shop page fallback added

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { GetStaticProps } from "next";
-import { Head } from "next/document";
+import Head from "next/head";
 import { useState } from "react";
 import { InitialLayout } from "../../Components/Layouts/InitialLayout";
 import { ShopLayout } from "../../Components/Layouts/ShopLayout";
@@ -18,7 +18,9 @@ const Shop = ({ products }) => {
 
    return (
       <InitialLayout>
-         <Head>Shop - Sunnah Station</Head>
+         <Head>
+            <title>Shop - Sunnah Station</title>
+         </Head>
          <div className=" pt-28 md:pt-32">
             <ShopLayout>
                <ShopProducts
