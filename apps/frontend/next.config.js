48c87ebfc87e5405ef999ac3e428e3e6b9ba9468
reasat-rafio <rafioar96@gmail.com>
2022-05-06 08:36:04 +0600

    :wrench: configs and json files tweaks

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,10 +1,4 @@
-const STUDIO_REWRITE = {
-  source: "/studio/:path*",
-  destination:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3333/studio/:path*"
-      : "/studio/index.html",
-};
+/** @type {import('next').NextConfig} */
 module.exports = {
-  rewrites: () => [STUDIO_REWRITE],
+  reactStrictMode: true,
 };
